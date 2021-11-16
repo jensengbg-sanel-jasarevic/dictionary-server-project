cf2cc308d6364c253ef865e8c154d6c394031473
SJ <sj@sj.com>
2021-11-16 12:54:58 +0100

    init server

---
+++ controller/api.js
@@ -0,0 +1,12 @@
+// Required modules 
+const express = require("express");
+
+const server = express() 
+
+// Middlewares
+
+
+// API Endpoints
+
+
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,6 @@
+const server = require("./controller/api");
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {    
+    console.log(`Server running on port ${port}`)
+})
