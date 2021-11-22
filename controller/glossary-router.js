adc7c547551cf9e2580b979b2e23c57a222379ff
SJ <sj@sj.com>
2021-11-22 13:56:35 +0100

    api endpoints for glossary

--- controller/glossary-router.js
+++ controller/glossary-router.js
@@ -2,10 +2,10 @@ const queries = require("../model/queries.js")
 const express = require ("express");
 
 const router = express.Router() 
-// test
-// GET glossary words
-router.get("/", async (req, res) => {
-    queries.readGlossary()
+
+// GET words
+router.get("/words", async (req, res) => {
+    queries.readWords()
     
     .then(success => { 
         res.status(200).json(success)
@@ -15,8 +15,25 @@ router.get("/", async (req, res) => {
     });
 })
 
-// POST glossary word
-router.post("/", async (req, res) => {
+// GET word
+router.get("/:word", async (req, res) => { 
+    await queries.readWord(req.params.word)
+
+    .then(success => {
+        if (success.length > 0) {
+            res.status(200).json({ message: "Operation successful"});
+        } 
+        else { 
+            res.status(404).json({ message: "Record not found" });
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: "Unable to perform operation" });
+    });
+});
+
+// POST word
+router.post("/:word", async (req, res) => {
     queries.createWord( {} ) 
     // Method is passed a argument as a value.
     // Method receives a value as a parameter.
