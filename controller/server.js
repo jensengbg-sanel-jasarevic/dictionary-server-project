3b12dd436add14ef4dcfd0f7a1df0720c4275415
SJ <sj@sj.com>
2021-12-08 01:51:33 +0100

    heroku

--- controller/server.js
+++ controller/server.js
@@ -1,11 +1,5 @@
-const port = process.env.PORT || 5000;
-
 // Required modules
 const express = require("express");
-
-// Start using Express framework
-const server = express();
-
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const userRouter = require("../controller/user-router.js");
@@ -13,12 +7,16 @@ const dictionaryRouter = require("../controller/dictionary-router.js");
 const commentsRouter = require("../controller/comments-router.js");
 const passport = require("passport");
 
+// Start using Express framework
+const server = express();
+
 // Middlewares
 server.use(cors()); // Allows AJAX requests to skip the Same-origin policy and access resources from remote hosts
 server.use(express.json()); // Parses JSON. On 'request' object a 'body' object containing the parsed data is populated
 server.use(cookieParser()); // used to parse the cookie from frontend
 server.use(passport.initialize());
 server.use(passport.session());
+
 // API endpoints
 server.get("/", (req, res) => {
   res.send("Server API");
